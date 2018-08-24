8918da8466fd921056ffaaec948c762fe76c2446
dnceglia <d.n.ceglia@hotmail.com>
2018-08-24 16:01:32 -0400

    creation of allEnemies array

--- js/app.js
+++ js/app.js
@@ -1,38 +1,53 @@
-function Enemy() {
-    this.x = 1;
-    this.y = 1;
+// ======================
+
+function Enemy(x, y) {
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
 
     // Draw the enemy on the screen, required method for game
     Enemy.prototype.render = function() {
-        ctx.drawImage(Resources.get(this.sprite), this.x = 1, this.y = 60);
-    };  
+        ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+    };
+
+    // update() {
+    //     this.edgeX = this.x > __;
+    //     this.edgeY = this.y < __;
+    // }
+
+    // changeSpeed() {
+
+    // }
 };
 
 function Player() {
     this.x = 2;
-    this.y = 2;
+    this.y = 5;
     this.player = 'images/char-boy.png'
 
     // update() {
-
+    //     did collision happen - x,y coords contact
     // }
 
     Player.prototype.render = function() {
-        ctx.drawImage(Resources.get(this.player), this.x = 200, this.y = 400);
+        ctx.drawImage(Resources.get(this.player), this.x * 101, this.y * 83);
     }
 
     // handleInput() {
 
     // }
+
+    // check 4 win
+
+    // if contacted - go back to first grass row
 }
 
-let enemy = new Enemy();
-// let enemy2 = new Enemy();
-// let enemy3 = new Enemy();
 let player = new Player();
-// const allEnemies = [];
+const allEnemies = [...Array(3)].map((_,i)=> new Enemy(0,i+1));
+
+
 // ======================================================================
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
