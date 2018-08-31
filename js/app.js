e5944e5db84a76dbcaffbb9ee6563ec6912dc481
dnceglia <d.n.ceglia@hotmail.com>
2018-08-30 20:15:34 -0400

    creation of modal

--- js/app.js
+++ js/app.js
@@ -10,6 +10,122 @@ var random = function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+var modal = document.querySelector('#modal');
+
+/* StopWatch function from Udacity scholar Ryan Waite's "Script Store" -
+   https://github.com/ryanwaite28/script-store/blob/master/js/stop-watch.js */
+   const StopWatch = function StopWatch() {
+    const self = this;
+  
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+  
+    let timer;
+    let on = false;
+  
+    self.startTimer = function(callback) {
+      if(on === true) { return; }
+      on = true;
+      timer = setInterval(function(){
+        seconds++;
+        if(seconds === 60) {
+          seconds = 0;
+          minutes++;
+          if(minutes === 60) {
+            minutes = 0;
+            hours++;
+          }
+        }
+        if(callback && callback.constructor === Function) {
+          callback();
+        }
+      }, 1000);
+      console.log('timer started');
+    }
+  
+    self.stopTimer = function() {
+      clearInterval(timer);
+      on = false;
+      console.log('timer ended: ', self.getTimeString());
+    }
+  
+    self.resetTimer = function() {
+      self.stopTimer();
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    }
+  
+    self.getTimeString = function() {
+      let hour = hours > 9 ? String(hours) : '0' + String(hours);
+      let minute = minutes > 9 ? String(minutes) : '0' + String(minutes);
+      let second = seconds > 9 ? String(seconds) : '0' + String(seconds);
+      let timeString = hour + ':' + minute + ':' + second;
+      return timeString;
+    }
+  
+    self.getHours = function() {
+      return hours;
+    }
+  
+    self.getMinutes = function() {
+      return minutes;
+    }
+  
+    self.getSeconds = function() {
+      return seconds;
+    }
+}
+
+// creation of a new instance of the StopWatch() function
+let watch = new StopWatch();
+
+// function to begin the timer and output the time in the HTML
+function startGameTimer() {
+    watch.startTimer(function() {
+        timer.innerText = watch.getTimeString();
+    });
+}
+
+// function to stop the timer and output the time in HTML
+function stopGameTimer() {
+    watch.stopTimer(function() {
+        timer.innerText = watch.getTimeString();
+    });
+}
+
+// function implementing the modal
+function showModal() {
+    modal.showModal();
+    stopGameTimer();
+
+    var modalTime = document.querySelector('#modal-time');
+    modalTime.innerHTML = 'Time Taken: ' + watch.getTimeString();
+}
+
+// function to close the modal
+function closeModal() {
+    modal.close();
+}
+
+// function holding the methods to restart the game
+// function resetGame() {
+//     allMatches = 0;
+//     cardArray = [];
+
+//     closeModal();
+//     resetTime();
+//     resetAfterWin();
+//     shuffleTheDeck();
+
+
+// }
+
+// function win() {
+
+// }
+
 // referenced Rodrick Bloomfield's introduction webinar
 // https://zoom.us/recording/play/aulotDlzKFegQFIJTaTzKgWvNkVsYtlwO454vL1UPE1Cm6lOUBQCtfVurPOIAGAS?startTime=1529542978000
 class Enemy {
@@ -106,10 +222,6 @@ let bug2 = new Enemy(-5, 2);
 let bug3 = new Enemy(-9, 3);
 const allEnemies = [];
 allEnemies.push(bug1,bug2,bug3);
-
-// win condition
-
-// win modal
 
 
 // This listens for key presses and sends the keys to your
